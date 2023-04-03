88c99b9add32501f94fe7dee94a18328b3051062
Jakub Sadowski <33390661+cruznovsky@users.noreply.github.com>
2023-04-03 14:48:11 +0200

    Add files via upload

---
+++ final-project.sql
@@ -0,0 +1,13 @@
+SELECT 	f.origin,
+		f.dest,
+		CAST(AVG(f.distance) AS decimal(5, 2)) AS avg_distance,
+		CEIL(COUNT(*) / 10) AS avg_flights,
+		CEIL(SUM(p.seats) / 10) AS avg_seats,
+		CAST(AVG(f.arr_delay) AS decimal(5,2)) AS avg_arr_delay
+FROM fly.flights AS f
+	LEFT JOIN fly.planes AS p
+		ON f.tailnum = p.tailnum
+WHERE f.distance BETWEEN 300 AND 400
+GROUP BY origin, dest
+HAVING avg_flights >= 5000
+ORDER BY avg_flights DESC;
